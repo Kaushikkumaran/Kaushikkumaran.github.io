0f4990e858b5532bfcf7a55bfd78f7f9b39decb6
Kaushikkumaran <kaushikkumaran@gmail.com>
2021-07-18 13:35:45 -0500

    Updates

---
+++ precache-manifest.13c1c4e9532845119e68796eb7062aac.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1665bfc1410ba825ad97bae085d8fda0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82cd7d773e00ef72880a",
+    "url": "/static/css/3.c7b31c95.chunk.css"
+  },
+  {
+    "revision": "3b265f654c307095870a",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "a277f92721df29d62427",
+    "url": "/static/css/main.b0ddd6a2.chunk.css"
+  },
+  {
+    "revision": "fc0a9c6a4acdf4164593",
+    "url": "/static/js/2.cb77f319.chunk.js"
+  },
+  {
+    "revision": "82cd7d773e00ef72880a",
+    "url": "/static/js/3.9ec06e5d.chunk.js"
+  },
+  {
+    "revision": "3b265f654c307095870a",
+    "url": "/static/js/4.3a445628.chunk.js"
+  },
+  {
+    "revision": "a277f92721df29d62427",
+    "url": "/static/js/main.54d000ef.chunk.js"
+  },
+  {
+    "revision": "d98468e414366563ad59",
+    "url": "/static/js/runtime-main.db517869.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "3952f41bd410b0f9b33c3242dc17ccda",
+    "url": "/static/media/Coursera.3952f41b.png"
+  },
+  {
+    "revision": "2e11bff47f839371144d34c3bfa42933",
+    "url": "/static/media/Data_Science_Coach.2e11bff4.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e3566fa2654da3f6767a5db6bb92f1f6",
+    "url": "/static/media/PySpark.e3566fa2.jpeg"
+  },
+  {
+    "revision": "f7a2102159d302cde2ee40bcf2c1c618",
+    "url": "/static/media/R_Programming.f7a21021.jpeg"
+  },
+  {
+    "revision": "79d8a714da535b14796069bba9e45979",
+    "url": "/static/media/Recognition.79d8a714.jpg"
+  },
+  {
+    "revision": "1bc5123b1668ecae42b0277a9325849f",
+    "url": "/static/media/Retail.1bc5123b.png"
+  },
+  {
+    "revision": "7ae65fcf53a19d724f9cc1606cac0fc3",
+    "url": "/static/media/Stat.7ae65fcf.jpeg"
+  },
+  {
+    "revision": "e708ac33f6e3fce9cd14b53bee5047f3",
+    "url": "/static/media/academic_excellence.e708ac33.png"
+  },
+  {
+    "revision": "8128f5885d6668232eb515eef05784ef",
+    "url": "/static/media/bostonRent.8128f588.jpeg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "8cd06b25c73dcd89097105d6bb9601e8",
+    "url": "/static/media/cpg.8cd06b25.jpeg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "70d5acc99cb2aeb6dc66aa17e309c9f0",
+    "url": "/static/media/power_bi.70d5acc9.jpeg"
+  },
+  {
+    "revision": "d437af1b649dcc7abd9e3efc8488f28b",
+    "url": "/static/media/sirMVIT.d437af1b.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
